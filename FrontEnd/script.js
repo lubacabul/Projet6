4ad078dc334287228925d7870823f599b16b0c6e
Karine Leclavier <lubacabul@gmail.com>
2023-12-10 17:27:19 +0100

    FEAT : fetch des catégories et filtrage des projets + modification du fetch des projets

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,56 +1,64 @@
-const portfolio = document.querySelector("portfolio")
-const galleryElement = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
+const filters = document.querySelectorAll(".filter");
+let works = [];
+let categories = [];
 
+function WorksImport() {
+    fetch("http://localhost:5678/api/works")
+        .then((res) => res.json())
+        .then((data) => {
+            works = data;
+            generateWorks(works);
+            displayModal(works);
+        });
+}
+WorksImport();
 
-// Récupération des projets de l'architecte//
-let dataApi = fetch("http://localhost:5678/api/works");
+function categoriesImport() {
+    fetch("http://localhost:5678/api/categories")
+        .then((res) => res.json())
+        .then((data) => {
+            categories = data;
+        });
+}
+categoriesImport();
 
-dataApi.then (async(responseData)=> {
-const response = await responseData.json();
-console.log(response[1]);
+function generateWorks(worksArray) {
+    gallery.innerHTML = "";
 
-// Test récupération de l'élèment 1 de la gallerie
-try{
-    const id = response[1].id;
-    const title = response[1].title;
-    const imageURL = response[1].imageURL;
+    worksArray.forEach((work) => {
+        const figure = document.createElement("figure");
+        gallery.appendChild(figure);
+        figure.classList = work.category.name;
+        figure.setAttribute("data-id", work.id);
 
-    console.log(id);
-    console.log(title);
-    console.log(imageURL)  
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+        figure.appendChild(img);
+
+        const figcaption = document.createElement("figcaption");
+        figcaption.innerHTML = work.title;
+        figure.appendChild(figcaption);
+    });
 }
 
-catch(err){
-        console.log(err);
-    }
-});
+function worksFilter() {
+    filters.forEach((filter) => {
+        const filterValue = filter.textContent;
 
-    // Récupère les données de l'API
-    fetch('http://localhost:5678/api/works')
-        .then(response => response.json())
-        .then(data => {
-            
-            // Pour chaque projet dans les données, crée une figure avec une image et un titre
-            data.forEach(project => {
-                const figureElement = document.createElement('figure');
-                figureElement.classList.add('project');
-
-                const imgElement = document.createElement('img');
-
-                // Crée les 2 élements dans figure (l'image et le titre)
-                imgElement.src = project.imageUrl;
-                imgElement.alt = project.title;
-
-                const figcaptionElement = document.createElement('figcaption');
-                figcaptionElement.textContent = project.title;
-
-                // Ajoute l'image et le titre à la figure
-                figureElement.appendChild(imgElement);
-                figureElement.appendChild(figcaptionElement);
-
-                // Ajoute la figure à la galerie
-                galleryElement.appendChild(figureElement);
-            });
-        })
-        .catch(error => console.error('Erreur lors de la récupération des données de l\'API :', error));
+        filter.addEventListener("click", () => {
+            let filteredWorks = [];
+            if (filterValue === "Tous") {
+                filteredWorks = works;
+            } else {
+                filteredWorks = works.filter(
+                    (work) => work.category.name === filterValue
+                );
+            }
+            generateWorks(filteredWorks);
+        });
+    });
+}
+worksFilter();
 
